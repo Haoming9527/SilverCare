3bab674bfd86bd1eb159c603fe85ef411cda85d8
Shen Haoming <167516473+Haoming9527@users.noreply.github.com>
2026-01-29 09:53:30 +0800

    init commit

---
+++ SilverCare-Backend/src/main/java/com/example/SilverCare_Backend/SilverCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SilverCare_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverCareBackendApplication.class, args);
+	}
+
+}
